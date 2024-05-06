c3e6e4fcf66412763564e5519e66c7b9ea1aff74
Kasula Saitejachary <98974519+kasulasaitejachary@users.noreply.github.com>
2024-05-06 06:07:36 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const Index = ()=>{
+const Root = ()=>{
+    return (
     <>
     <App />
     </>
+  )
 }
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<Root />, document.getElementById("root"));
